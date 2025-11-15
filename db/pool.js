d07651de69e816139b865215c7bfb872cfd1944a
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2025-11-15 09:51:12 +0100

    Changed object attributes for pool from connectionString to user, database, password, etc.

--- db/pool.js
+++ db/pool.js
@@ -3,10 +3,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  // host: process.env.HOST,
-  // user: process.env.USER,
-  // database: process.env.DB,
-  // password: process.env.PASSWORD,
-  // port: process.env.PORT,
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.PGHOST,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.PGPORT,
+  // connectionString: process.env.DATABASE_URL,
 });
